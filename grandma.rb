8197e161eadae9a2f0b10a6ae9ebd9c89cdf00d1
Robin Kim <rkim1214@gmail.com>
2018-10-09 02:43:48 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,11 @@
 # Write a speak_to_grandma method.
 
 def speak_to_grandma(phrase)
-  if 
+  if phrase == ""
+    "I LOVE YOU TOO PUMPKIN!"
     "HUH?! SPEAK UP, SONNY!"
   elsif
+    "NO, NOT SINCE 1938!"
     
 end 
 
